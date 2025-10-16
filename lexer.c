d272871c3505016b208dd950091eaaf6d7b827f6
Maitathe-b <maiathenoob@gmail.com>
2025-10-16 19:06:41 +0100

    lexer recovered

--- lexer.c
+++ lexer.c
@@ -1,7 +1,41 @@
+#include "parser.h"
 
-#include "minishell.h"
 
-cat | ls
+static size_t	ft_count_args(char const *s, char c, int quotes)
+{
+	size_t	i;
+	size_t	isarg;
+	size_t	args;
 
-cat -1-1 l
+	args = 0;
+	i = 0;
+	isarg = 0;
+	while (s[i])
+	{
+		if (s[i] != c && isarg == 0)
+		{
+			args++;
+			isarg++;
+		}
+		if (s[i] == c && isarg == 1)
+			isarg--;
+		i++;
+	}
+	return (args);
+}
 
+char	**lexer(char *input)
+{
+	int		i;
+	int		quotes;
+	char	**cmdtable;
+	int		arg;
+	
+	i = 0;
+	arg = 0;
+	while (input[i++])
+	{
+
+	}
+	
+}
