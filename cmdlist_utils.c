c3153025817e9f92a4b55e24db1ab599c7065ca7
Maitathe-b <maiathenoob@gmail.com>
2025-10-22 18:34:37 +0100

    new cmd finished, not tested

--- cmdlist_utils.c
+++ cmdlist_utils.c
@@ -3,7 +3,9 @@
 t_redir *get_redir(char **cmd, int i)
 {
 	t_redir	*ret;
+	int	j;
 
+	j = 0;
 	while (cmd[i][0] != '|')
 	{
 		if (cmd[i][0] != '>' || cmd[i][0] != '<')
@@ -13,16 +15,42 @@ t_redir *get_redir(char **cmd, int i)
 			ret = malloc(sizeof(t_redir));
 			if (!ret)
 				return (NULL);
-			ret->token = ft_strdup(cmd[i]);
-			ret->file = ft_strdup(cmd[i + 1]);
+			ret->token[j] = ft_strdup(cmd[i]);
+			ret->file[j] = ft_strdup(cmd[i + 1]);
+			j++;
 		}
+		i++;
 	}
-	return (NULL);
+	return (ret);
 }
 
-static char	*rem_redir(char **cmd)
+static int	final_cmd_size(char **cmd, int i)
 {
-	
+	int	count;
+	while(cmd[i][0] != '|')
+	{
+		if(cmd[i][0] == '<' || cmd[i][0] == '>')
+			i = i + 2;
+		count++;
+	}
+	return(count);
+}
+
+static char	**final_cmd(char **cmd, int i)
+{
+	char	**final_cmd;
+	int		cmd_size;
+	int		j;
+
+	j = 0;
+	cmd_size = final_cmd_size(cmd, i);
+	cmd = malloc(cmd_size * sizeof(char *));
+	while(cmd[i][0] != '|')
+	{
+		if(cmd[i][0] == '<' || cmd[i][0] == '>')
+			i = i + 2;
+		final_cmd[j++] = ft_strdup(cmd[i++]);
+	}
 }
 
 t_cmdlist *new_cmd(char **cmd, int i)
@@ -32,9 +60,9 @@ t_cmdlist *new_cmd(char **cmd, int i)
 	new = malloc(sizeof(t_cmdlist));
 	if (!new)
 		return (NULL);
-	new->command = cmd;
-	new->input = NULL;
-	new->output = NULL;
+	new->input = get_redir(cmd, i);
+	new->output = get_redir(cmd, i);
+	new->command = final_cmd(cmd, i);
 	new->next = NULL;
 	return (new);
 }
