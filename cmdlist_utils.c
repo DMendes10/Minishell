8a58a1d833e1a2e376e53fcd2039c807265e868d
Maitathe-b <maiathenoob@gmail.com>
2025-12-05 15:21:07 +0000

    parsing fixed

--- cmdlist_utils.c
+++ cmdlist_utils.c
@@ -15,15 +15,15 @@ static int	final_cmd_size(char **cmd, int i)
 	int	count;
 
 	count = 0;
-	while(cmd[i] && cmd[i][0] != '|')
+	while (cmd[i] && cmd[i][0] != '|')
 	{
-		if(cmd[i][0] == '<' || cmd[i][0] == '>')
+		if (cmd[i][0] == '<' || cmd[i][0] == '>')
 			i++;
 		else
 			count++;
 		i++;
 	}
-	return(count);
+	return (count);
 }
 
 static char	**final_cmd(char **cmd, int i)
@@ -35,16 +35,15 @@ static char	**final_cmd(char **cmd, int i)
 	j = 0;
 	cmd_size = final_cmd_size(cmd, i);
 	final_cmd = ft_calloc((cmd_size + 1), sizeof(char *));
-	while(cmd[i] && cmd[i][0] != '|')
+	while (cmd[i] && cmd[i][0] != '|')
 	{
-		if(cmd[i][0] == '<' || cmd[i][0] == '>')
+		if (cmd[i][0] == '<' || cmd[i][0] == '>')
 			i++;
 		else
 			final_cmd[j++] = ft_strdup(cmd[i]);
 		i++;
 	}
-	// final_cmd[j] = NULL;
-	return(final_cmd);
+	return (final_cmd);
 }
 
 void	ft_cmd_add_back(t_cmdlist **stack, t_cmdlist *node)
@@ -63,7 +62,7 @@ void	ft_cmd_add_back(t_cmdlist **stack, t_cmdlist *node)
 	node->next = NULL;
 }
 
-t_cmdlist *new_cmd(char **cmd, int i)
+t_cmdlist	*new_cmd(char **cmd, int i)
 {
 	t_cmdlist	*new;
 
@@ -79,4 +78,3 @@ t_cmdlist *new_cmd(char **cmd, int i)
 	new->next = NULL;
 	return (new);
 }
-
